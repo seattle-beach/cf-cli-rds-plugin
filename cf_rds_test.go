194451a05f9a74a0f8211323e95c0d6b616463c5
Esther Ng <eng@pivotal.io>
2017-05-18 10:05:24 -0700

    adding basic ginko test suite

---
+++ cf_rds_test.go
@@ -0,0 +1,14 @@
+package main_test
+
+import (
+	// . "github.com/seattle-beach/cf-cli-rds-plugin"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CfRds", func() {
+	It("Builds a basic plugin", func() {
+		Expect(true).To(BeTrue())
+	})
+})
