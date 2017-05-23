2d41903668241e5245dddefa1b96d2a28749a852
Luke Winikates <lwinikates@pivotal.io>
2017-05-23 10:06:24 -0700

    Use BeforeEach to remove test duplication
    Signed-off-by: Aditya Anchuri <aanchuri@pivotal.io>

--- cf_rds/cf_rds_test.go
+++ cf_rds/cf_rds_test.go
@@ -10,27 +10,26 @@ import (
 var _ = Describe("CfRds", func() {
 
 	Describe("BasicPlugin", func() {
-		Context("with uri option", func() {
-			It("creates a user-provided service with user-provided RDS instance", func() {
-				conn := &pluginfakes.FakeCliConnection{}
-				ui := &MockUi{}
+		FContext("with uri option", func() {
+			var ui MockUi
+			var conn *pluginfakes.FakeCliConnection
+
+			BeforeEach(func() {
+				conn = &pluginfakes.FakeCliConnection{}
+				ui = MockUi{}
+
 				p := &BasicPlugin{
-					UI: ui,
+					UI: &ui,
 				}
 				args := []string{"aws-rds", "create", "name", "--uri", "postgres://user:pwd@example.com:5432/database"}
 				p.Run(conn, args)
-				cliCommandArgs := conn.CliCommandArgsForCall(0)
-				Expect(cliCommandArgs).To(Equal([]string{"cups", "name", "-p", "{\"uri\":\"postgres://user:pwd@example.com:5432/database\"}"}))
+			})
+
+			It("creates a user-provided service with user-provided RDS instance", func() {
+				Expect(conn.CliCommandArgsForCall(0)).To(Equal([]string{"cups", "name", "-p", "{\"uri\":\"postgres://user:pwd@example.com:5432/database\"}"}))
 			})
 
 			It("displays success message", func() {
-				conn := &pluginfakes.FakeCliConnection{}
-				ui := &MockUi{}
-				p := &BasicPlugin{
-					UI: ui,
-				}
-				args := []string{"aws-rds", "create", "name", "--uri", "postgres://user:pwd@example.com:5432/database"}
-				p.Run(conn, args)
 				Expect(ui.TextTemplate).To(Equal("SUCCESS"))
 			})
 		})
@@ -46,6 +45,6 @@ func (u *MockUi) DisplayText(template string, data ...map[string]interface{}) {
 	u.TextTemplate = template
 }
 
-func (u MockUi) DisplayError(err error) {
+func (u *MockUi) DisplayError(err error) {
 	panic("NOT IMPLEMENTED")
 }
